29be3aef1eafa3b7670cc811dc378f73f7d7cc82
d1gi <digital_boy@mail.ru>
2015-09-24 19:52:51 +0700

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -vR ./distr/bin ~/
-cp -v ./distr/.config ~/.config
+cp -vR ./distr/.config/ ~/.config/
 cp -v ./distr/.bashrc ~/.bashrc
 cp -v ./distr/.gitconfig ~/.gitconfig
 cp -v ./distr/.gitignore ~/.gitignore
